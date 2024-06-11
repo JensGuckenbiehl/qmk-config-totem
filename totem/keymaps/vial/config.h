1317e856479b6630569d8655e832b9d765d9da34
Jens Guckenbiehl <jens@werdorf.com>
2024-06-11 11:55:02 +0200

    Added vial keymap

---
+++ totem/keymaps/vial/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD3, 0xDB, 0x47, 0x41, 0xE2, 0x30, 0xF4, 0x35}
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+
+//#define TAPPING_TERM 170
+#define TAPPING_TERM 170
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
